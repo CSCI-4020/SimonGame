c4dc90e1d2e0715196bb40e46ccfde929180a5e9
Chad <mscott4@my.apsu.edu>
2016-03-07 23:48:21 -0600

    AboutActivity added

---
+++ app/src/main/java/edu/apsu/csci/simongame/AboutActivity.java
@@ -0,0 +1,15 @@
+package edu.apsu.csci.simongame;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
