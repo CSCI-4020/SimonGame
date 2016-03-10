20ed0ea3cbad048cd1c6df81792311dc4d5a6ed3
fmiller8 <fmiller8@my.apsu.edu>
2016-03-09 23:53:27 -0600

    inserted buttons having issue with alingmnet chad can you try to aling these for me and maybe resizing these images im having so much trouble with this

--- app/src/main/java/edu/apsu/csci/simongame/AboutActivity.java
+++ app/src/main/java/edu/apsu/csci/simongame/AboutActivity.java
@@ -2,6 +2,7 @@ package edu.apsu.csci.simongame;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
     private String about;
@@ -11,6 +12,9 @@ public class AboutActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView tv = (TextView)findViewById(R.id.about_textView);
+        tv.append("\n\n\n Created by Frank Miller and Micheal Scott");
     }
 
 }
